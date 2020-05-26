c51d5c801d5f1979e5e9b197acc59acf80b6ae8c
hikari_asano <hikari.asano@drecom.co.jp>
2020-05-26 23:15:49 +0900

    recommit

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,4 +1,3 @@
-jQuery('.nav_sp').on('click',function(){
-    jQuery('.menu__line').toggleClass('active');
-    jQuery('.gnav').fadeToggle();
+$('.menu-btn').on('click', function(){
+    $('.menu').toggleClass('is-active');
 });
